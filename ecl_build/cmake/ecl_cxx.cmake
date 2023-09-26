1d1466ee1821f64502ba33d603d80e9945c96ba7
Sebastian Peralta <peraltas@seas.upenn.edu>
2023-09-26 10:10:32 -0400

    removed Werror

--- ecl_build/cmake/ecl_cxx.cmake
+++ ecl_build/cmake/ecl_cxx.cmake
@@ -26,7 +26,7 @@ endmacro()
 # Enable the kitchen sink, i.e. as much as possible.
 macro(ecl_enable_cxx_warnings)
   if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-    add_compile_options(-Wall -Wextra -Werror -Wpedantic)
+    add_compile_options(-Wall -Wextra -Wpedantic)
   endif()
 endmacro()
 
